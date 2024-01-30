cc664966713f83b922f55d3f472d2f2b2d466ff2
Carlos Segarra <carlos@carlossegarra.com>
2024-01-30 11:37:33 +0000

    faasm: add network-bound part to the main lammps loop

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,7 @@ int totalNumLoops = 3;
 // use a user provided value (and we modify the global variable), this changes
 // won't be persisted across migrations.
 int checkEvery = 1;
+int numNetLoops = -1;
 LAMMPS* lammps = nullptr;
 
 void doLammps()
@@ -54,10 +55,35 @@ void doLammps()
     }
 }
 
+void doAllToAll(int rank, int worldSize, int nLoops)
+{
+    int chunkSize = 2;
+    int fullSize = worldSize * chunkSize;
+
+    // Arrays for sending and receiving
+    int sendBuf[fullSize];
+    int expected[fullSize];
+    int actual[fullSize];
+
+    // Populate data
+    for (int i = 0; i < fullSize; i++) {
+        // Send buffer from this rank
+        sendBuf[i] = (rank * 10) + i;
+
+        // Work out which rank this chunk of the expectation will come from
+        int rankOffset = (rank * chunkSize) + (i % chunkSize);
+        int recvRank = i / chunkSize;
+        expected[i] = (recvRank * 10) + rankOffset;
+    }
+
+    for (int i = 0; i < nLoops; i++) {
+        MPI_Alltoall(
+          sendBuf, chunkSize, MPI_INT, actual, chunkSize, MPI_INT, MPI_COMM_WORLD);
+    }
+}
+
 void doBenchmark(int nLoops)
 {
-    // bool mustCheck = nLoops == totalNumLoops;
-
     if (globalArgc == -1 || globalArgv == nullptr) {
         printf("Error! LAMMPS is a nullptr!\n");
     }
@@ -72,13 +98,16 @@ void doBenchmark(int nLoops)
     for (int i = 0; i < nLoops; i++) {
         // Barrier to make sure all ranks are in sync (including those that
         // have been migrated)
-        printf("Rank %i/%i executing loop %i\%i\n", rank, worldSize, i + 1, nLoops);
+        printf("Rank %i/%i executing loop %i/%i\n", rank, worldSize, i + 1, nLoops);
         MPI_Barrier(MPI_COMM_WORLD);
-        // printf("Rank %i/%i exitting first barrier!\n", rank, worldSize);
 
+        // Compute-intensive part of the benchmark
         lammps = new LAMMPS(globalArgc, globalArgv, MPI_COMM_WORLD);
         doLammps();
-        // if (mustCheck && ((i + 1)  % checkEvery == 0) && ((i + 1) != totalNumLoops)) {
+
+        // Network-intensive part of the benchmark
+        doAllToAll(rank, worldSize, numNetLoops);
+
         // Check for migration opportunities if this iteration is a multiple of
         // check every, and it is not the last iteration. Bear in mind that the
         // meaning of "last" iteration will vary between migrated and non-
@@ -91,10 +120,8 @@ void doBenchmark(int nLoops)
                 printf("---------------------------------------------------\n");
             }
 #endif
-            printf("Rank %i/%i in mig. check branch (iter: %i\%i)\n", rank, worldSize, i + 1, nLoops);
-            // printf("Rank %i/%i entering second barrier!\n", rank, worldSize);
+            printf("Rank %i/%i in mig. check branch (iter: %i/%i)\n", rank, worldSize, i + 1, nLoops);
             MPI_Barrier(MPI_COMM_WORLD);
-            // printf("Rank %i/%i entering second barrier!\n", rank, worldSize);
 #ifdef __faasm
             __faasm_migrate_point(&doBenchmark, (nLoops - i - 1));
 #endif
@@ -113,26 +140,45 @@ int main(int argc, char **argv)
     globalArgv = argv;
     globalArgc = argc;
 
+    // To get the benchmark parameters, we use Faasm's input data, or env.
+    // variables for native MPI (we must pass them using mpirun -x ENV -x ENV2)
 #ifdef __faasm
     long inputSize = faasmGetInputSize();
     uint8_t* inputBuffer = (uint8_t*) malloc(inputSize * sizeof(uint8_t));
     faasmGetInput(inputBuffer, inputSize);
 
+    // Faasm's configuration has three parameters:
+    // - numLoops: how many iterations does the benchmark do (each iteration
+    //             consists of a compute and a network bound part)
+    // - checkEvery: how often do we check for migration opportunities
+    // - numNetLoops: how many iterations do we do in the network-bound part
+    //                of the main experiment loop
+    //  We pass them separated by a space: "{} {} {}"
     char* inputStr = (char*) inputBuffer;
     int checkEveryIn = atoi(strtok(inputStr, " "));
-    int numLoopsIn = atoi(strtok(NULL, " "));
+    int numLoopsIn = atoi(strtok(inputStr, " "));
+    int numNetLoopsIn = atoi(strtok(NULL, " "));
+#else
+    char* inputStr = getenv("FAASM_BENCH_PARAMS");
 
-    // Filthy hack to set the check period without modifying the function
-    // signature. Note that the migrated functions won't see the updated
-    // value as we don't migrate global variables, but that's OK as we don't
-    // support migrating a function twice.
+    // For the native configuration, checkEvery is always equal to numLoops
+    // (as we can never migrate native jobs), so we only need to pass two
+    // values separated by a colon
+    int numLoopsIn = atoi(strtok(inputStr, ":"));
+    int checkEveryIn = numLoopsIn;
+    int numNetLoopsIn = atoi(strtok(NULL, ":"));
+#endif
+
+    // Hacky way to pass the input data (not LAMMPS argc/arv) to the benchmark
     int* numTotalLoopsPtr = &totalNumLoops;
     *numTotalLoopsPtr = numLoopsIn;
     int* checkEveryPtr = &checkEvery;
     *checkEveryPtr = checkEveryIn;
-#endif
+    int* numNetLoopsPtr = &numNetLoops;
+    *numNetLoopsPtr = numNetLoopsIn;
+
     printf(
-      "Starting MPI migration v2 checking at iter %i/%i\n", checkEvery, totalNumLoops);
+      "Starting MPI migration v3 checking at iter %i/%i (%i net loops)\n", checkEvery, totalNumLoops, numNetLoops);
 
     doBenchmark(totalNumLoops);
 }
